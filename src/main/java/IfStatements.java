74f4f4508e99aa54535ac61fb8a310d3d466f534
RazaDon Willrich <razradical93@gmail.com>
2025-03-05 10:19:21 +0000

    Completed If Statement.

--- src/main/java/IfStatements.java
+++ src/main/java/IfStatements.java
@@ -12,7 +12,11 @@ public class IfStatements {
      * @return - x if bool is true, otherwise return y.
      */
     public int exercise1(boolean bool, int x, int y) {
-        return 0;
+        if (bool) {
+            return x;
+        } else {
+            return y;
+        }
     }
 
     /**
@@ -22,7 +26,13 @@ public class IfStatements {
      * @return - "Positive", "Negative", or "Zero" depending on the input.
      */
     public String exercise2(int x) {
-        return null;
+        if (x > 0) {
+            return "Positive";
+        } else if (x < 0) {
+            return "Negative";
+        } else {
+            return "Zero";
+        }
     }
 
     /**
@@ -40,6 +50,14 @@ public class IfStatements {
      * @return - "Yes", "No", or "Not quite!" depending on the input. 
      */
     public String exercise3(int year) {
-        return null;
+        if (year % 4 != 0) {
+            return "No";
+        } else if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0) {
+            return "Not quite";
+        } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+            return "Yes";
+        } else {
+            return "Yes";
+        }
     }
 }
